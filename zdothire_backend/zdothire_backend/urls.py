043aa50195adccfdd583b5588414f3c4454b1c4b
Anilkumar-tech101 <anil.b@station-s.org>
2025-10-25 18:51:17 +0530

    frontend added

--- zdothire_backend/zdothire_backend/urls.py
+++ zdothire_backend/zdothire_backend/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from zdothire import views  # 👈 import your app’s views, not django.views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),  
     path('api/', include('zdothire.urls')), 
 ]
-
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
